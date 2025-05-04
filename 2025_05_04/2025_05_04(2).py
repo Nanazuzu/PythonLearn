2d966cef3d8890ee6706e2377725117c9ab8aa6a
nanazuzu <emps1308@gmail.com>
2025-05-04 14:40:27 +0900

    2025_05_04, python algorithm with over 10

--- 2025_05_04/2025_05_04(2).py
+++ 2025_05_04/2025_05_04(2).py
@@ -2,21 +2,29 @@ def decompress(s: str) -> str:
   jud = [str(x) for x in range(10)]
   lst = []
   mid_str = []
+  mid_num = []
   for index in s:
     if index in jud:
-      if len(mid_str) == 0:
-        lst.append(index)
-      else:
+      if len(mid_str) != 0:
         lst.append(''.join(mid_str))
         mid_str.clear()
-        lst.append(index)
+        mid_num.append(index)
+      else:
+        mid_num.append(index)
     elif (index == '(') or (index == ')'):
       if index == ')' and len(mid_str) != 0:
         lst.append(''.join(mid_str))
         mid_str.clear()
+      elif index == '(' and len(mid_num) != 0:
+        lst.append(''.join(mid_num))
+        mid_num.clear()
       continue
     else:
       mid_str.append(index)
+      if len(mid_num) != 0:
+        lst.append(''.join(mid_num))
+        mid_num.clear()
+  print(lst)
   mid_str = lst
   string = ""
   while(len(mid_str)):
@@ -29,3 +37,4 @@ def decompress(s: str) -> str:
 print(decompress("3(ab)"))
 print(decompress("2(a2(b))"))
 print(decompress("3(x2(yz))"))
+print(decompress("10(ab2(c12(y)))"))
